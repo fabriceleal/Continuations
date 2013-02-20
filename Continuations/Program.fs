aed637004234ab7bcee12edd301de53af3a6350e
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2013-02-20 21:59:59 +0000

    a little exercise

--- Continuations/Program.fs
+++ Continuations/Program.fs
@@ -27,7 +27,14 @@ let max_k x y k =
 let print_d (x : int) = 
     printfn "%d" x;;
 
-
+// this:
+let g n = n + 1
+let f n = g(n + 1) + 1
+printfn "%d" (f(1) + 1)
+// becomes:
+let g_k n k = k(n + 1)
+let f_k n k = g_k(n + 1) (fun x -> k(x + 1))
+f_k 1 (fun x -> printfn "%d" (x + 1))
 
 
 [<EntryPoint>]
@@ -36,6 +43,7 @@ let main argv =
     // Close enough?    
     max 1 2 |> print_d
 
+    // More CPS Styl-ish
     max_k 1 2 print_d
 
 
