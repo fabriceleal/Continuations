e5cfbefdd69447052fc495e598404f64ec56da65
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2013-02-23 21:24:37 +0000

    count_nodes in a binary tree

--- Continuations/Program.fs
+++ Continuations/Program.fs
@@ -97,17 +97,29 @@ let rec nth_k n (ls : 'a list) k =
         k(Some(ls.Head))
     else
         nth_k (n - 1) ls.Tail k
-        
+
+type Tree =
+    | Node of Tree * Tree
+    | Leaf
+
+// node_count
+let rec node_count = function
+                    | Node(lt, rt) -> 1 + node_count(lt)  + node_count(rt)
+                    | Leaf -> 0;;
+
+let rec node_count_k tree k = match tree with
+                                | Node(ltree, rtree) ->
+                                    let new_k1 = (fun ltree_count -> 
+                                        let new_k2 = (fun rtree_count -> 
+                                            k(1 + ltree_count + rtree_count)
+                                        )
+                                        node_count_k rtree new_k2
+                                    )
+                                    node_count_k ltree new_k1
+                                | Leaf -> k 0
 
 [<EntryPoint>]
 let main argv = 
-
-    // Close enough?    
-    max 1 2 |> print_d
-
-    // More CPS Styl-ish
-    max_k 1 2 print_d
-
     // this:
     let g n = n + 1
     let f n = g(n + 1) + 1
@@ -117,35 +129,78 @@ let main argv =
     let f_k n k = g_k(n + 1) (fun x -> k(x + 1))
     f_k 1 (fun x -> printfn "CPS: %d" (x + 1))
 
+
+    // EXAMPLE max
+
+    // Close enough?    
+    max 1 2 |> print_d
+
+    // More CPS Styl-ish
+    max_k 1 2 print_d
+    // -------------------
+
+    // EXAMPLE factorial
+    let fact_n = 5
+
     // this:
-    factorial 5 |> printfn "Normal Factorial of %d: %d" 5
+    factorial fact_n |> printfn "Normal Factorial of %d: %d" fact_n
+    
     // becomes:
-    factorial_k 5 (fun x -> printfn "CPS Factorial of %d: %d" 5 x) |> ignore
+    factorial_k fact_n (fun x -> printfn "CPS Factorial of %d: %d" fact_n x) |> ignore
+    // -------------------
+
+    // EXAMPLE sum
+    let sum_n = 5
+
+    // this:
+    sum sum_n |> printfn "Normal sum of %d: %d" sum_n
+
+    // becomes:
+    sum_k sum_n (fun t -> printfn "CPS sum of %d: %d" sum_n t)
+    // -------------------
+
+    // EXAMPLE fibo
+    let fibo_n = 9
     
     // this:
-    sum 5 |> printfn "Normal sum of %d: %d" 5
+    fibo fibo_n |> printfn "Normal fibonacci %d: %d" fibo_n
+    
     // becomes:
-    sum_k 5 (fun t -> printfn "CPS sum of %d: %d" 5 t)
+    fibo_k fibo_n (fun x -> printfn "CPS fibonacci %d: %d" fibo_n x)
+    // -------------------
+
+    // EXAMPLE nth
+    let ls, i1, i2 = [1;2;3;4;5;6], 3, 15
+    
+    // this:
+    match (nth i1 ls) with
+    | Some(n) -> printfn "Normal %dth: %d" i1 n
+    | None -> printfn "Normal %dth: List not big enough!" i1
+
+    match (nth i2 ls) with
+    | Some(n) -> printfn "Normal %dth: %d" i2 n
+    | None -> printfn "Normal %dth: List not big enough!" i2
+
+    // becomes:    
+    nth_k i1 ls (function
+                | Some(n) -> printfn "CPS %dth: %d" i1 n
+                | None -> printfn "CPS %dth: List not big enough!" i1)
+
+    nth_k i2 ls (function
+                | Some(n) -> printfn "CPS %dth: %d" i2 n
+                | None -> printfn "CPS %dth: List not big enough!" i2)
+    // -------------------
+
+    // EXAMPLE count_nodes
+    let t = Node(Node(Leaf, Leaf), Node(Leaf, Node(Leaf, Node(Leaf, Leaf))))
 
     // this:
-    fibo 9 |> printfn "Normal fibonacci %d: %d" 9
-    // becomes:
-    fibo_k 9 (fun x -> printfn "CPS fibonacci %d: %d" 9 x)
+    node_count t |> printfn "Normal nodes: %d"
 
-    // this:
-    match (nth 2 [1; 2; 3; 4; 5; 6]) with
-    | Some(n) -> printfn "Normal %dth: %d" 2 n
-    | None -> printfn "Normal %dth: List not big enough!" 2
-    match (nth 15 [1; 2; 3; 4; 5; 6]) with
-    | Some(n) -> printfn "Normal %dth: %d" 15 n
-    | None -> printfn "Normal %dth: List not big enough!" 15
     // becomes:
-    nth_k 3 [1; 2; 3; 4; 5; 6] (function
-                                | Some(n) -> printfn "CPS %dth: %d" 2 n
-                                | None -> printfn "CPS %dth: List not big enough!" 2)
-    nth_k 15 [1; 2; 3; 4; 5; 6] (function
-                                | Some(n) -> printfn "CPS %dth: %d" 15 n
-                                | None -> printfn "CPS %dth: List not big enough!" 15)
+    node_count_k t (fun count -> printfn "CPS nodes: %d" count)
+
+    // -------------------
 
     // wait
     Console.ReadKey() |> ignore
